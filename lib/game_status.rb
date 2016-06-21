338631a183fb8f4497f6f6f3f4b37d8f5554c7ba
blessedsoy <unmigong@gmail.com>
2016-06-21 16:16:38 -0400

    Tic Tac Toe Game Status methods

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,56 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+  WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+
+def won?(board)
+
+  WIN_COMBINATIONS.each do |arr|
+
+    one = arr[0]
+    two = arr[1]
+    three = arr[2]
+
+    if board[one] == board[two] && board[two] == board[three] && board[one] != " "
+        return arr
+    end
+  end
+  return false
+end
+
+
+def full?(board)
+
+  board.each do |space|
+    if space == " " || space == "  "
+      return false
+    end
+  end
+return true
+end
+
+
+def draw?(board)
+won?(board) == false && full?(board) ? true : false
+end
+
+def over?(board)
+  if won?(board) != false || draw?(board) || full?(board)
+    return true
+  else 
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board) != false
+    arr = won?(board)
+  return board[arr[0]]
+end
+end
+
+
+
+
